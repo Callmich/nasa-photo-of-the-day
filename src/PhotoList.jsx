395ed6f94cc650ba2b8515994a59c9af791cad71
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-19 14:02:33 -0700

    added props to pass down and commented out axios call in order to stop re-requesting API pulls

--- src/PhotoList.jsx
+++ src/PhotoList.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 import PhotoCard from "./PhotoCard";
 
 function PhotoList() {
 
+    const [photos, setPhotos] = useState([]);
+    const [date, useDate] = useState("2020-02-19")
+
+    useEffect(()=> {
+        axios
+            // .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`)
+            .then(response => {
+                console.log(response.data);
+                setPhotos(response.data);
+            })
+            .catch(error => [console.log("No Data", error)]);
+    }, []);
 
     return (
         <div>
-            <PhotoCard />
+            {photos.map(pic => {
+                return (
+                    <PhotoCard 
+                        picture={pic.url}
+                        date={pic.date}
+                        title={pic.title}
+                        fact={pic.explanation}
+                    />
+                )
+            })}
         </div>
     )
 
