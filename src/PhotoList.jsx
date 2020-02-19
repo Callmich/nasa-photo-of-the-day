fad329396f0414916a8a9244767751c7b01f08ed
Michael Callahan <CallahanMichaelE@gmail.com>
2020-02-19 13:29:02 -0700

    added npm install as well as axios. set outline of Photo List and Photo card. currently have photolist rendering on app. Will move to buld photocard next.

---
+++ src/PhotoList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PhotoList() {
+
+
+    return (
+        <div>
+            <p>This is the Photo List</p>
+        </div>
+    )
+
+};
+
+export default PhotoList;
